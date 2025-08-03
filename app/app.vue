83e04e9944a4963ddb82f8f544739326561f870a
María Fernández <matefe1706@gmail.com>
2025-08-03 23:33:17 +0200

    test(app): add fetch logic for homepage and products

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,49 @@
+<script setup lang="ts">
+interface Product {
+  id: number
+  title: string
+  slug: string
+  createdAt: string
+  updatedAt: string
+  publishedAt: string
+}
+const { find, findOne } = useStrapi()
+
+// Find single type (single type)
+
+const { data: home } = await find('home-page', {
+  populate: {
+    image: {
+      fields: ['id', 'url', 'alternativeText'],
+    },
+  },
+})
+console.log('Home Page Data:', home)
+
+// Find all products (collection)
+
+const { data: products } = await find('products')
+console.log('Products Data:', products)
+
+// Find just one product by slug (collection)
+
+const productSlug = 'product-1'
+
+const { data: product } = await findOne<Product>('products', undefined, {
+  filters: {
+    slug: {
+      $eq: productSlug,
+    },
+  },
+})
+console.log('Product:', product)
+</script>
+
 <template>
   <div>
+    {{ products }}
     <NuxtRouteAnnouncer />
     <NuxtWelcome />
+    {{ home }}
   </div>
 </template>
