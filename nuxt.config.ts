8ebe2752484d89f291421d718d58b15e105e82db
María Fernández <matefe1706@gmail.com>
2025-08-06 18:00:57 +0200

    chore(config): add pinia and i18n

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import i18nConfig from './config/i18n'
+
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
@@ -10,8 +12,19 @@ export default defineNuxtConfig({
     '@nuxt/test-utils',
     '@nuxt/ui',
     '@nuxtjs/strapi',
+    '@pinia/nuxt',
+    '@nuxtjs/i18n',
   ],
   ssr: false,
+  i18n: {
+    ...i18nConfig,
+    detectBrowserLanguage: {
+      useCookie: true,
+      cookieKey: 'i18n_redirected',
+      redirectOn: 'root',
+      fallbackLocale: 'es',
+    },
+  },
   strapi: {
     url: process.env.STRAPI_URL,
     prefix: '/api',
