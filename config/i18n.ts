9ad983044ec1ac1c08513c7e323d3117eb825076
María Fernández <matefe1706@gmail.com>
2025-08-06 17:44:06 +0200

    feat(config): add pages and i18n config

---
+++ config/i18n.ts
@@ -0,0 +1,11 @@
+import type { ModuleOptions } from '@nuxtjs/i18n'
+import { pages } from './i18n/pages'
+
+export default {
+  lazy: true,
+  defaultLocale: 'es',
+  locales: [{ code: 'es', file: '../../locales/es.json', language: 'es' }],
+  strategy: 'prefix_and_default',
+  customRoutes: 'config',
+  pages,
+} as Partial<ModuleOptions>
---
+++ config/i18n/pages.ts
@@ -0,0 +1,14 @@
+export const pages = {
+  'about-me': {
+    es: '/sobre-mi',
+  },
+  projects: {
+    es: '/proyectos',
+  },
+  contact: {
+    es: '/contacto',
+  },
+  'thank-you': {
+    es: '/gracias',
+  },
+}
