be74bca277dcac688fba7993575ab59c89bf09d2
María Fernández <matefe1706@gmail.com>
2025-09-20 17:05:18 +0200

    feat(layout): add default layout

---
+++ app/layouts/default.vue
@@ -0,0 +1,35 @@
+<script lang="ts" setup>
+useHead({
+  link: [
+    { rel: 'preconnect', href: 'https://www.google.com' },
+    { rel: 'preconnect', href: 'https://maps.googleapis.com' },
+  ],
+})
+</script>
+
+<template>
+  <UiNavbarMainNavbar />
+  <div class="layout">
+    <main class="content">
+      <slot />
+    </main>
+    <UiFooterMainFooter />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.layout {
+  height: calc(100dvh - 5.9063rem);
+  margin: 5.9063rem 0 0 0;
+  @include flex(column, flex-start, flex-start);
+  @include responsive() {
+    margin: 4rem 0 0;
+  }
+}
+
+.content {
+  max-width: 120rem;
+  width: 100%;
+  flex: 1;
+}
+</style>
