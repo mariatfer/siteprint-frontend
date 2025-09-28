27771a387037c4dbfde86ddf0ab552dc8faf0984
María Fernández <matefe1706@gmail.com>
2025-09-28 17:10:50 +0200

    feat(pages): add product pages

--- app/pages/productos/index.vue
+++ app/pages/productos/index.vue
@@ -1,62 +1,39 @@
 <script setup lang="ts">
+import { getProductsPage } from '@/services/products'
+import { getCategories } from '@/services/category'
 import type { ProductsPage } from '@/interfaces/products'
-import type { Category } from '~/interfaces/navbar'
+import type { Category } from '@/interfaces/navbar'
 
-const { find } = useStrapi()
-const { data: response } = await find<ProductsPage>('product-page', {
-  populate: {
-    hero: {
-      populate: {
-        heroImage: {
-          fields: ['url', 'alternativeText'],
-        },
-      },
-    },
-    seo: {
-      populate: {
-        ogImage: {
-          fields: ['url', 'alternativeText'],
-        },
-      },
-    },
-  },
-})
-
-const productData = response as unknown as ProductsPage
-
-const { data: categories } = await find<Category>('categories', {
-  populate: {
-    products: {
-      populate: {
-        productImage: {
-          fields: ['url', 'alternativeText'],
-        },
-      },
-    },
-  },
-})
+const productData = ref<ProductsPage>()
+productData.value = await getProductsPage()
+const categories = ref<Category[]>()
+categories.value = await getCategories()
 
 const { contrastColor, analyzeImageColor } = useImageColor()
 
 onMounted(async () => {
-  const imgUrl = `http://localhost:1337${productData.hero.heroImage.url}`
-  await analyzeImageColor(imgUrl, 100)
+  if (productData.value) {
+    const imgUrl = `http://localhost:1337${productData.value.hero.heroImage.url}`
+    await analyzeImageColor(imgUrl, 100)
+  }
 })
 
 useSeoMeta({
-  title: productData.seo.metaTitle,
-  ogTitle: productData.seo.ogTitle,
-  description: productData.seo.metaDescription,
-  ogDescription: productData.seo.ogDescription,
-  ogImage: productData.seo.ogImage,
-  ogImageAlt: productData.seo.ogImage.alternativeText,
-  author: productData.seo.author,
+  title: productData.value.seo.metaTitle,
+  ogTitle: productData.value.seo.ogTitle,
+  description: productData.value.seo.metaDescription,
+  ogDescription: productData.value.seo.ogDescription,
+  ogImage: productData.value.seo.ogImage,
+  ogImageAlt: productData.value.seo.ogImage.alternativeText,
+  author: productData.value.seo.author,
+  robots: `${productData.value.seo.noIndex ? 'noindex' : 'index'}, ${productData.value.seo.noFollow ? 'nofollow' : 'follow'}`,
 })
 </script>
 
 <template>
-  <UiBreadCrumbs :color="contrastColor" :position-absolute="true" />
-  <UiTheHero :hero="productData.hero" />
+  <UiSEOTitle v-if="productData?.seo.metaTitle" :meta-title="productData.seo.metaTitle" />
+  <UiBreadCrumbs :color="contrastColor" :position-absolute="true" :no-padding="true" />
+  <UiTheHero v-if="productData?.hero" :hero="productData.hero" />
   <ViewsProductsCategoryGrid
     v-for="(category, index) in categories"
     :key="category.id"
