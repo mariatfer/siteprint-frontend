589b3d9f97ca07d9185ae1c056f260c777481f81
María Fernández <matefe1706@gmail.com>
2025-09-28 17:25:15 +0200

    feat(constants): add file types, icons, links and pages to constants

---
+++ app/constants/files.ts
@@ -0,0 +1,31 @@
+export const MAX_SIZE_MB = 5
+export const ALLOWED_TYPES = [
+  'application/pdf',
+  'image/png',
+  'image/jpeg',
+  'application/vnd.corel-draw',
+  'application/msword',
+  'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+  'application/vnd.oasis.opendocument.text',
+  'text/plain',
+  'application/rtf',
+  'application/x-msworks',
+  'application/wordperfect',
+  'application/octet-stream',
+]
+
+export const ALLOWED_EXTENSIONS = [
+  'pdf',
+  'png',
+  'jpeg',
+  'jpg',
+  'cdr',
+  'doc',
+  'docx',
+  'odt',
+  'txt',
+  'rtf',
+  'wps',
+  'wks',
+  'wpd',
+]
